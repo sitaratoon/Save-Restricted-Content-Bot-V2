fbf2d68a8e2fbf9f2025d3dea5b4465694a03aab
sitaratoon <ajay393798@gmail.com>
2025-01-29 09:05:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "18946488"))
+API_HASH = getenv("API_HASH", "c163d4e28e63196c3806cf3b9b2885de")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6692613520").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://sitaratoons:sitaratoons@cluster0.98nq3.mongodb.net/?retryWrites=true&w=majority")
 LOG_GROUP = getenv("LOG_GROUP", "")
 CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
