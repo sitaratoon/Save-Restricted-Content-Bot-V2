3120b6be685b147466455cc6ca1fe2d7fcb59742
sitaratoon <ajay393798@gmail.com>
2025-06-05 21:19:14 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -74,8 +74,8 @@ async def token_handler(client, message):
     user_id = message.chat.id
     if len(message.command) <= 1:
         image_url = "https://i.postimg.cc/v8q8kGyz/startimg-1.jpg"
-        join_button = InlineKeyboardButton("Join Channel", url="https://t.me/team_spy_pro")
-        premium = InlineKeyboardButton("Get Premium", url="https://t.me/kingofpatal")   
+        join_button = InlineKeyboardButton("Update Channel", url="https://t.me/ST_Rename_Update")
+        premium = InlineKeyboardButton("Support Group", url="https://t.me/Anime_Chats_St")   
         keyboard = InlineKeyboardMarkup([
             [join_button],   
             [premium]    
